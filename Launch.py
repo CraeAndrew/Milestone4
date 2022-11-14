73bb201abe240c8edd7d3ce1e0e5f9910332df2c
CraeAndrew <115107926+CraeAndrew@users.noreply.github.com>
2022-11-14 14:50:16 -0500

    Update Launch.py

--- Launch.py
+++ Launch.py
@@ -5,10 +5,29 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     ld = LaunchDescription()
+
+    joy_node = Node(
+        package="joy",
+        executable="joy_node",
+        #parameters=[{"steering_offset": 0.0}],
+    )
+    ld.add_action(joy_node)
+
+    joy = Node(
+        package="mile4",
+        executable="joy",
+        #parameters=[{"Up": 0.0},
+        #            {"Ui": 0.0},
+        #            {"Ud": 0.0}
+        #]
+    )
+    ld.add_action(joy)
+
     motor_controller_node = Node(
         package="motor_driver",
         executable="motor_controller",
-        parameters=[{"steering_offset": 0.0, "limiter": True}],
+        parameters=[{"steering_offset": 0.0, "limiter": False}],
     )
     ld.add_action(motor_controller_node)
+
     return ld
