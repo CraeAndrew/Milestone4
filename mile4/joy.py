550c6ff338b92ad49afa0e64d1926ca1444d6f3d
supikc <88164157+supikc@users.noreply.github.com>
2022-11-18 14:46:08 -0500

    Update joy.py

--- mile4/joy.py
+++ mile4/joy.py
@@ -4,6 +4,7 @@ from rclpy.node import Node
 from sensor_msgs.msg import Joy
 
 from std_msgs.msg import Int16
+from std_msgs.msg import float32
 
 from drive_interfaces.msg import VehCmd
 
@@ -64,6 +65,7 @@ class Joy_Count(Node):
         e = self.r-y
         
         self.publisher3.publish(e)
+        self.publisher4.publish(self.r)
         
         kp = 0.1
         ki = 0.0
@@ -88,7 +90,6 @@ class Joy_Count(Node):
         control.steering_angle = self.r2
         
         self.publisher2.publish(control)
-        self.publisher4.publish(self.throttle_effort)
 
 
 def main(args=None):
