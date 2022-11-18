81268c9783383f5095b748fd66f2ab682f0c41ad
supikc <88164157+supikc@users.noreply.github.com>
2022-11-18 14:54:58 -0500

    Update joy.py

--- mile4/joy.py
+++ mile4/joy.py
@@ -37,8 +37,8 @@ class Joy_Count(Node):
         self.publisher = self.create_publisher(Int16, 'led_color', 10)
         self.publisher2 = self.create_publisher(VehCmd, 'vehicle_command_angle', 10)
         
-        self.publisher3 = self.create_publisher(Float32, 'error', 10)
-        self.publisher4 = self.create_publisher(Float32, 'command', 10)
+        #self.publisher3 = self.create_publisher(Float32, 'error', 10)
+        #self.publisher4 = self.create_publisher(Float32, 'command', 10)
         
     def timer_callback(self):
         
@@ -64,10 +64,10 @@ class Joy_Count(Node):
 
         e = self.r-y
         
-        err = Float32()
-        err.data = e
-        cmd = Float32()
-        cmd.data = self.r
+       # err = Float32()
+        #err.data = e
+        #cmd = Float32()
+        #cmd.data = self.r
         
         kp = 0.1
         ki = 0.0
@@ -92,8 +92,8 @@ class Joy_Count(Node):
         control.steering_angle = self.r2
         
         self.publisher2.publish(control)
-        self.publisher3.publish(err)
-        self.publisher4.publish(cmd)
+        #self.publisher3.publish(err)
+        #self.publisher4.publish(cmd)
 
 
 def main(args=None):
